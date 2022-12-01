d5bdfc6b7ff83c55ae6bb5242b80a92a9083894f
Tomer <morsky2@gmail.com>
2022-12-02 01:06:37 +0200

    fix style

--- client/src/components/HomeScreen/home-screen.tsx
+++ client/src/components/HomeScreen/home-screen.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState } from "react";
 import UsernameTextField from "../UserNameTextField/username-text-field";
 import UserPasswordTextField from "../PasswordTextField/password-text-field";
 import styles from './home-screen.module.scss'
+import Logo from "../Logo/logo";
 
 const HomeScreen: FC = () => {
 
@@ -10,7 +11,9 @@ const HomeScreen: FC = () => {
 
     return (
         <div className={styles.homeScreenContainer}>
+            <Logo></Logo>
             <UsernameTextField userName={username} onUsernameChanged={setUsername}/>
+            <br/>
             <UserPasswordTextField userPassword={password} onPasswordChanged={setPassword}/>
         </div>
     )
---
+++ client/src/components/Logo/logo.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import styles from './logo.module.scss'
+
+const Logo: FC = () =>{
+
+    return (
+        <div className={styles.logoContainer}>
+            LOGO
+        </div>
+    )
+}
+
+export default Logo;
