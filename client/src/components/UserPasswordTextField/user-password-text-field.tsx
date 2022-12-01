a557d665b2c7b0b1eb494e8bd773299c773177a1
Tomer <morsky2@gmail.com>
2022-12-01 19:37:42 +0200

    hide and show password

--- client/src/components/UserPasswordTextField/user-password-text-field.tsx
+++ client/src/components/UserPasswordTextField/user-password-text-field.tsx
@@ -1,28 +1,48 @@
-import TextField from "@mui/material/TextField";
-import React, { FC } from "react";
+import { FormControl, InputLabel, Input, InputAdornment, IconButton } from "@mui/material";
+import React, { FC, useState } from "react";
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 
 interface UserPasswordTextFieldProps {
     userPassword: string;
     onPasswordChanged: (updatedUserPassword: string) => void
 }
 
-const UserPasswordTextField: FC<UserPasswordTextFieldProps> = ({userPassword, onPasswordChanged}) => {
-    
+const UserPasswordTextField: FC<UserPasswordTextFieldProps> = ({ userPassword, onPasswordChanged }) => {
+
+    const [showPassword, setShowPassword] = useState(false);
+
     const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const password = event.target.value
         onPasswordChanged(password)
-    }
+    };
+
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword)
+    };
 
     return (
-        <div>
-            <TextField
-                value={userPassword}
-                onChange={handleTextChange}
-                label="password"
-                type="password"
-                variant="standard"
-            />
-        </div>
+        <>
+            <FormControl variant="standard">
+                <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                <Input
+                    id="standard-adornment-password"
+                    type={showPassword ? 'text' : 'password'}
+                    value={userPassword}
+                    onChange={handleTextChange}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}>
+                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                />
+            </FormControl>
+        </>
     )
 
     //TODO - add validations on username field, if needed
